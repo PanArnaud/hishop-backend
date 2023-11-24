819b1479a69283a735aaae37db9116a39ad3a14b
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2023-11-24 14:03:49 +0400

    remove useless comments

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,6 @@
 const functions = require("firebase-functions");
 
 exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
-  // Stripe initialization
-  // Data must contains the ids and quantity of each item.
-  // Then we fetch this ids from the product collection and fill the line_items
-
   const stripe = require("stripe")(functions.config().stripe.secret_key);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
