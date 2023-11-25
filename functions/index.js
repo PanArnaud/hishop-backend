b918f308707edf96c15c4f2ae98bc74802ab46d4
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2023-11-25 15:37:33 +0400

    Update cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 const functions = require("firebase-functions");
 
+const DOMAIN = functions.config().domains.prod;
+
 exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
   const stripe = require("stripe")(functions.config().stripe.secret_key);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: data.success_url,
-    cancel_url: data.cancel_url,
+    success_url: DOMAIN + "/?success=1",
+    cancel_url: DOMAIN + data.cancel_url,
     line_items: data.line_items,
   });
 
